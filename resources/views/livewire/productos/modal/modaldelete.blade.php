51294e7f0f5d2db227d01e2d935ce3b3510c2c43
alcidesc <alcideskaceres1990@gmail.com>
2023-08-25 10:51:22 -0400

    modales-wiev-delete

---
+++ resources/views/livewire/productos/modal/modaldelete.blade.php
@@ -0,0 +1,19 @@
+<div class="modal fade" id="exampleModal{{$pro->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Eliminar producto</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>¿Realmente quiere eliminar la producto {{ $pro->nombre }}?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                <button type="button" wire:click="delete({{ $pro->id }})" class="btn btn-danger" data-dismiss="modal">Eliminar</button>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/livewire/productos/modal/modalwievproducto.blade.php
@@ -0,0 +1,120 @@
+<div class="modal fade" wire:ignore.self id="viewModal{{$pro->id}}" tabindex="-1" role="dialog" aria-labelledby="viewModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">{{ $pro->nombre }}</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    @if($pro->foto)
+                        <div class="col-md-12" align="center">
+                            <img src="{{ asset('/images/productos/'.$pro->foto) }}" style="width:200px;border-radius:20px;">
+                        </div>
+                    @endif
+                    <div class="col-md-4">
+                        <label>Código de barra:</label>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-barcode"></i></span>
+                            </div>
+                            <input type="text" class="form-control" value="{{$pro->codigo}}" readonly>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <label>Nombre del producto:</label>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-shopping-bag"></i></span>
+                            </div>
+                            <input type="text" class="form-control" value="{{$pro->nombre}}" readonly>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <label>Enlace del producto:</label>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-code"></i></span>
+                            </div>
+                            <input type="text" class="form-control" value="{{$pro->slug}}" readonly>
+                        </div>
+                    </div>
+                    @if($pro->descripcion)
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label for="informacion">Descripción del producto</label>
+                                <?= $pro->descripcion ?>
+                            </div>
+                        </div>
+                    @endif
+                    <div class="col-md-4"><br>
+                        <label>Precio de venta:</label>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-money-bill-wave"></i></span>
+                            </div>
+                            <input type="number" value="{{$pro->precio}}" class="form-control" readonly>
+                        </div>
+                    </div>
+                    @if($pro->precio2)
+                    <div class="col-md-4"><br>
+                        <label>Precio de venta 2:</label>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-money-bill-wave"></i></span>
+                            </div>
+                            <input type="number" value="{{$pro->precio2}}" class="form-control" readonly>
+                        </div>
+                    </div>
+                    @endif
+                    @if($pro->precio3)
+                    <div class="col-md-4"><br>
+                        <label>Precio de venta 3:</label>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-money-bill-wave"></i></span>
+                            </div>
+                            <input type="number" value="{{$pro->precio3}}" class="form-control" readonly>
+                        </div>
+                    </div>
+                    @endif
+                    @if($pro->oferta)
+                    <div class="col-md-3"><br>
+                        <label>Precio oferta:</label>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-money-bill-wave"></i></span>
+                            </div>
+                            <input type="number" value="{{$pro->oferta}}" class="form-control" readonly>
+                        </div>
+                    </div>
+                    @endif
+                    <div class="col-md-4"><br>
+                        <label>Stock disponible:</label>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-layer-group"></i></span>
+                            </div>
+                            <input type="number" value="{{$pro->stock}}" class="form-control" readonly>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="form-group" wire:ignore>
+                            <label for="informacion">Seleccione Categorias</label><br>
+                            <ul>
+                                @foreach($categorias as $value)
+                                    <li>{{ $value->nombre }}</li>
+                                @endforeach
+                            </ul>  
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+    </div>
+</div>
