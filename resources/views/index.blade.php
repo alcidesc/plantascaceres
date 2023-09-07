fe1ef5fd5f8bec1050a0c768b743135e7870098b
Richardopy <richar.saucedo@gmail.com>
2023-09-07 05:50:38 -0400

    Cambios varios

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,6 +24,7 @@
 		<!-- cart -->
 		<!-- for bootstrap working -->
 			<script type="text/javascript" src="{{asset('frontend/js/bootstrap-3.1.1.min.js')}}"></script>
+			<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css'>
 		<!-- //for bootstrap working -->
 		<link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 		<link href='//fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,900,900italic,700italic' rel='stylesheet' type='text/css'>
